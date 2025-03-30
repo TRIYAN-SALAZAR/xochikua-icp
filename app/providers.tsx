9f9000b107cd82b257cbeb196a85359398a3c19a
Im_JVallejo <46511000+Yukyshiram@users.noreply.github.com>
2025-03-29 21:16:27 -0600

    Next js + Hero UI configuration

---
+++ app/providers.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import * as React from "react";
+import { HeroUIProvider } from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProviders, ThemeProviderProps } from "next-themes";
+
+export interface ProviderProps{
+    children: React.ReactNode;
+    themeProps?: ThemeProviderProps;
+}
+
+export function Providers({ children, themeProps }: ProviderProps) {
+    const router = useRouter();
+
+    return (
+        <HeroUIProvider navigate={router.push}>
+            <NextThemesProviders {...themeProps}>
+                {children}
+            </NextThemesProviders>
+        </HeroUIProvider>
+    );
+}
