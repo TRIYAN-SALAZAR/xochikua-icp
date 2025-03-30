313eb768042e3a233cddbb1718bafaf25e1ffe0a
Im_JVallejo <46511000+Yukyshiram@users.noreply.github.com>
2025-03-30 02:10:19 -0600

    Delete Eslint

--- src/app/config/siteconfig.ts
+++ src/app/config/siteconfig.ts
@@ -2,8 +2,8 @@ export type Siteconfig = typeof siteConfig;
 
 export const siteConfig = {
     name: "Xochikua",
-    description: "",
-    url: "",
+    description: "Servicio de pago universal para Zapopan que permite realizar transacciones en una amplia variedad de servicios de manera rápida y segura.",
+    url: "xochikua.sklconnect.com",
     links: {
         github: "https://github.com/TRIYAN-SALAZAR/xochikua-icp",
     },
